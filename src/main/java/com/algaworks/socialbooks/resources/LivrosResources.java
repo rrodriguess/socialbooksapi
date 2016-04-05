e845e020f7df028631aa8f85aed41f04ed04d7cb
Renato Rodrigues <renato.mello@paulistano.org.br>
2016-04-05 17:20:51 -0300

    2.20 Feito com sucesso

--- src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
+++ src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -3,6 +3,8 @@ package com.algaworks.socialbooks.resources;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,7 +32,7 @@ public class LivrosResources {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> salvar(@RequestBody Livro livro) {
+	public ResponseEntity<Void> salvar(@Valid @RequestBody Livro livro) {
 		livro = livrosService.salvar(livro);
 
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(livro.getId()).toUri();
